a5edcc61dac8ef6a581a28e47939859ec281ce62
MARIO ALEJANDRO DOMINGUEZ SALGADO <madominguezs@liverpool.com.mx>
2026-09-09 11:39:04 -0600

    init migration backend

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module edd-panel-backend
+
+go 1.26.5
